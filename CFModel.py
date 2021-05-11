a17cb70992bcaee56de4ce94bd2e082d083d061f
alkhwarizmi <hamza.ben.ammar@outlook.com>
2021-05-11 17:05:43 +0200

    Add NCF model and minor edits in data.py and main.py

--- CFModel.py
+++ CFModel.py
@@ -3,9 +3,10 @@
 # Keras uses TensorFlow tensor library as the backend system to do the heavy compiling
 
 import numpy as np
-from keras.layers import Embedding, Reshape, Merge
-from keras.models import Sequential
-
+# from keras.layers import Embedding, Reshape, Merge
+# from keras.models import Sequential
+from keras.models import Model, Sequential
+from keras.layers import Embedding, Flatten, Input, merge, Dropout, Dense, BatchNormalization, Reshape, Merge
 
 class CFModel(Sequential):
 
@@ -33,3 +34,89 @@ class CFModel(Sequential):
     # The rate function to predict user's rating of unrated items
     def rate(self, user_id, item_id):
         return self.predict([np.array([user_id]), np.array([item_id])])[0][0]
+		
+		
+class NCFModel():
+
+    def __init__(self, n_users, m_items, k_factors, **kwargs):
+        '''
+        # MLP Embeddings
+        movie_vec_mlp = Sequential()
+        movie_vec_mlp.add(Embedding(input_dim=m_items, output_dim=k_factors, input_length=1, input_shape=[1]))
+        movie_vec_mlp.add(Reshape((k_factors,)))
+
+        user_vec_mlp = Sequential()
+        user_vec_mlp.add(Embedding(input_dim=n_users, output_dim=k_factors, input_length=1, input_shape=[1]))
+        user_vec_mlp.add(Reshape((k_factors,)))
+
+        # MF Embeddings
+        movie_vec_mf = Sequential()
+        movie_vec_mf.add(Embedding(input_dim=m_items, output_dim=k_factors, input_length=1, input_shape=[1]))
+        movie_vec_mf.add(Reshape((k_factors,)))
+
+        user_vec_mf = Sequential()
+        user_vec_mf.add(Embedding(input_dim=n_users, output_dim=k_factors, input_length=1, input_shape=[1]))
+
+        user_vec_mf.add(Reshape((k_factors,)))
+        # MLP layers
+        model_mlp = Sequential()
+        model_mlp.add(Merge([movie_vec_mlp, user_vec_mlp], mode='concat', dot_axes=1))
+        model_mlp.add(Dropout(0.2))
+        model_mlp.add(Dense(100, activation='relu'))
+        model_mlp.add(BatchNormalization())
+        model_mlp.add(Dropout(0.2))
+        model_mlp.add(Dense(50, activation='relu'))
+        model_mlp.add(BatchNormalization())
+        model_mlp.add(Dropout(0.2))
+        model_mlp.add(Dense(10, activation='relu'))
+
+        # Prediction from both layers
+        model_mf = Sequential()
+        model_mf.add(Merge([movie_vec_mf, user_vec_mf], mode='dot', dot_axes=1))
+        self.model_mlp_mf = Sequential()
+        self.model_mlp_mf.add(Merge([model_mf, model_mlp], mode='concat', dot_axes=1))
+
+        # Final prediction
+        self.model_mlp_mf.add(Dense(1, activation='relu'))
+        '''
+
+        # Define inputs
+        movie_input = Input(shape=[1], name='movie-input')
+        user_input = Input(shape=[1], name='user-input')
+
+        # MLP Embeddings
+        movie_embedding_mlp = Embedding(m_items, k_factors, name='movie-embedding-mlp')(movie_input)
+        movie_vec_mlp = Flatten(name='flatten-movie-mlp')(movie_embedding_mlp)
+
+        user_embedding_mlp = Embedding(n_users, k_factors, name='user-embedding-mlp')(user_input)
+        user_vec_mlp = Flatten(name='flatten-user-mlp')(user_embedding_mlp)
+
+        # MF Embeddings
+        movie_embedding_mf = Embedding(m_items, k_factors, name='movie-embedding-mf')(movie_input)
+        movie_vec_mf = Flatten(name='flatten-movie-mf')(movie_embedding_mf)
+
+        user_embedding_mf = Embedding(n_users, k_factors, name='user-embedding-mf')(user_input)
+        user_vec_mf = Flatten(name='flatten-user-mf')(user_embedding_mf)
+
+        # MLP layers
+        concat = merge([movie_vec_mlp, user_vec_mlp], mode='concat', name='concat')
+        concat_dropout = Dropout(0.2)(concat)
+        fc_1 = Dense(100, name='fc-1', activation='relu')(concat_dropout)
+        fc_1_bn = BatchNormalization(name='batch-norm-1')(fc_1)
+        fc_1_dropout = Dropout(0.2)(fc_1_bn)
+        fc_2 = Dense(50, name='fc-2', activation='relu')(fc_1_dropout)
+        fc_2_bn = BatchNormalization(name='batch-norm-2')(fc_2)
+        fc_2_dropout = Dropout(0.2)(fc_2_bn)
+
+        # Prediction from both layers
+        pred_mlp = Dense(10, name='pred-mlp', activation='relu')(fc_2_dropout)
+        pred_mf = merge([movie_vec_mf, user_vec_mf], mode='dot', name='pred-mf')
+        combine_mlp_mf = merge([pred_mf, pred_mlp], mode='concat', name='combine-mlp-mf')
+
+        # Final prediction
+        result = Dense(1, name='result', activation='relu')(combine_mlp_mf)
+
+        self.model_final = Model([user_input, movie_input], result)
+
+    def rate(self, user_id, item_id):
+        return self.model_final.predict([np.array([user_id]), np.array([item_id])])[0][0]
--- main.py
+++ main.py
@@ -32,6 +32,8 @@ ratings = shuffled_ratings['rating'].values
 
 # Define model
 model = CFModel(data.max_userid, data.max_movieid, K_FACTORS)
+# model_ncf = NCFModel(data.max_userid, data.max_movieid, K_FACTORS)
+# model = model_ncf.model_final
 model.compile(loss='mse', optimizer='adamax')
 
 # Callbacks monitor the validation loss, save the model weights each time the validation loss has improved
@@ -46,23 +48,31 @@ print('Minimum RMSE at epoch', '{:d}'.format(idx+1), '=', '{:.4f}'.format(math.s
 
 # Use the pre-trained model
 trained_model = CFModel(data.max_userid, data.max_movieid, K_FACTORS)
+# trained_model_obj = NCFModel(data.max_userid, data.max_movieid, K_FACTORS)
+# trained_model = trained_model_obj.model_final
 # Load weights
 trained_model.load_weights('weights.h5')
 
 # Predict user ratings (enter user and his recommended movies --> get rating)
 TEST_USER = 2000
 data.users[data.users['user_id'] == TEST_USER]
+
 def predict_rating(user_id, movie_id):
     return trained_model.rate(user_id - 1, movie_id - 1)
-user_ratings = data.ratings[ratings['user_id'] == TEST_USER][['user_id', 'movie_id', 'rating']]
+	# return trained_model_obj.rate(user_id - 1, movie_id - 1)
+
+# user_ratings = data.ratings[ratings['user_id'] == TEST_USER][['user_id', 'movie_id', 'rating']]
+user_ratings = data.ratings[data.ratings['user_id'] == TEST_USER][['user_id', 'movie_id', 'rating']]
 user_ratings['prediction'] = user_ratings.apply(lambda x: predict_rating(TEST_USER, x['movie_id']), axis=1)
-user_ratings.sort_values(by='rating', ascending=False).merge(movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
+# user_ratings.sort_values(by='rating', ascending=False).merge(movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
+user_ratings = user_ratings.sort_values(by='rating', ascending=False).merge(data.movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
 print(user_ratings)
 
 # Recommend user items (enter user and all movies --> get rating and sort them by best)
 recommendations = data.ratings[data.ratings['movie_id'].isin(user_ratings['movie_id']) == False][['movie_id']].drop_duplicates()
 recommendations['prediction'] = recommendations.apply(lambda x: predict_rating(TEST_USER, x['movie_id']), axis=1)
-recommendations.sort_values(by='prediction', ascending=False).merge(movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
+# recommendations.sort_values(by='prediction', ascending=False).merge(movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
+recommendations = recommendations.sort_values(by='prediction', ascending=False).merge(data.movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
 print(recommendations)
 
 # Afterwards, we put the above steps together and it will be used in the next section. The results are wrapped with Dataset and DataLoader.
