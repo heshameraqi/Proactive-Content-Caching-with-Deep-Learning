f4d22291dba96d04209905867c45f40f7814146d
heshameraqi <hesham.eraqi@gmail.com>
2021-05-30 20:16:20 +0200

    Update CFModel.py

--- CFModel.py
+++ CFModel.py
@@ -6,7 +6,7 @@ import numpy as np
 # from keras.layers import Embedding, Reshape, Merge
 # from keras.models import Sequential
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Embedding, Flatten, Input, merge, Dropout, Dense, BatchNormalization, Reshape, Merge
+from tensorflow.keras.layers import Embedding, Flatten, Input, Dropout, Dense, BatchNormalization, Reshape, Merge
 
 
 class CFModel(Sequential):
