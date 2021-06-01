9196d812cf322ddbcc827a514d70f94b92a95257
Hesham Eraqi <hesham.eraqi@NMG.local>
2021-06-01 21:21:32 +0200

    fixed model dependencies

--- simulate_cache.py
+++ simulate_cache.py
@@ -30,6 +30,7 @@ class SimModel:
         len_step = floor(len(self.movies)/nb_step)
         # we cut the ratings file into multiple intervals where the model will be trained in one interval and used in the next one
         for i in range(0, nb_step):
+            print("=============== Training stage %d ==============="%(i))
             inf_index = len_step * i
             # inf_index = 0  # in case we want to create intervals by going back each time to the beginning of the file
             if i == (nb_step-1):
@@ -42,11 +43,10 @@ class SimModel:
             if cf_flag:
                 model = CFModel(self.data.max_userid, self.data.max_movieid, K_FACTORS).model
             else:
-                model_ncf = NCFModel(self.data.max_userid, self.data.max_movieid, K_FACTORS).model
-                model = model_ncf.model_final
+                model = NCFModel(self.data.max_userid, self.data.max_movieid, K_FACTORS).model
             model.compile(loss='mse', optimizer='adamax')
             # Callbacks monitor the validation loss, save the model weights each time the validation loss has improved
-            callbacks = [EarlyStopping('val_loss', patience=2), ModelCheckpoint(f'weights{i+1}.h5', save_best_only=True)]
+            callbacks = [EarlyStopping('val_loss', patience=10), ModelCheckpoint(f'weights{i+1}.h5', save_best_only=True)]
             # Train the model: Use 30 epochs, 90% training data, 10% validation data
             inputs = np.transpose(np.vstack((par_users, par_movies)))
             history = model.fit(inputs, par_ratings, epochs=30, validation_split=.1, shuffle=True, batch_size=500, verbose=2, callbacks=callbacks)
@@ -59,19 +59,15 @@ class SimModel:
             min_val_loss, idx = min((val, idx) for (idx, val) in enumerate(history.history['val_loss']))
             print('Minimum RMSE at epoch', '{:d}'.format(idx + 1), '=', '{:.4f}'.format(math.sqrt(min_val_loss)))
 
-    @staticmethod
-    def predict_rating(trained_model, user_id, movie_id):
-        return trained_model.rate(user_id - 1, movie_id - 1)
-
     def apply_ncf_model(self, weights_file, cf_flag=True):
         # the default model will be ncf, cf will be used instead only if specified
         if cf_flag:
-            trained_model = CFModel(self.data.max_userid, self.data.max_movieid, K_FACTORS).model
+            trained_model = CFModel(self.data.max_userid, self.data.max_movieid, K_FACTORS)
             # Load weights
-            trained_model.load_weights(weights_file)
+            trained_model.model.load_weights(weights_file)
         else:
-            trained_model = NCFModel(self.data.max_userid, self.data.max_movieid, K_FACTORS).model
-            trained_model.model_final.load_weights(weights_file)
+            trained_model = NCFModel(self.data.max_userid, self.data.max_movieid, K_FACTORS)
+            trained_model.model.load_weights(weights_file)
         rec_movies_list_all = list()
         # If a movie is recommended for more than X (threshold) users, it will be cached
         # For every user TODO: should be every user in the past stages only not future
@@ -80,14 +76,14 @@ class SimModel:
             # Predict user i ratings (enter user and his recommended movies --> get rating)
             # print("data.ratings: \n", data.ratings)
             # user_ratings = self.data.ratings[self.data.ratings['user_id'] == i][['user_id', 'movie_id', 'rating']]
-            # user_ratings['prediction'] = user_ratings.apply(lambda x: SimModel.predict_rating(trained_model, i, x['movie_id']), axis=1)
+            # user_ratings['prediction'] = user_ratings.apply(lambda x: trained_model.rate(i, x['movie_id']), axis=1)
             # user_ratings = user_ratings.sort_values(by='rating', ascending=False).merge(self.data.movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
             # print(user_ratings)
             # Recommend user items (enter user and all movies --> get rating and sort them by best)
             # Remove from data.ratings the movies already rated/requested by the user and predict from the list of movies not yet rated
             user_ratings = self.data.ratings[self.data.ratings['user_id'] == i][['user_id', 'movie_id', 'rating']]
             recommendations = self.data.ratings[self.data.ratings['movie_id'].isin(user_ratings['movie_id']) == False][['movie_id']].drop_duplicates()
-            recommendations['prediction'] = recommendations.apply(lambda x: SimModel.predict_rating(trained_model, i, x['movie_id']), axis=1)
+            recommendations['prediction'] = recommendations.apply(lambda x: trained_model.rate(i, x['movie_id']), axis=1)
             recommendations = recommendations.sort_values(by='prediction', ascending=False).merge(self.data.movies, on='movie_id', how='inner', suffixes=['_u', '_m']).head(20)
             # print(recommendations)
             rec_movies_list_user = recommendations["movie_id"].tolist()
@@ -107,7 +103,8 @@ if __name__ == "__main__":
     # cache_size = 10
     sim_model = SimModel(K_FACTORS)
     sim_model.train_ncf_model(nb_step)
-    for i in range(0, nb_step):
+    '''for i in range(0, nb_step):
+        print("=============== Testing stage %d ==============="%(i))
         rec_movies_list_all = sim_model.apply_ncf_model(f'weights{i + 1}.h5')  # contains the recommended movies for each user
         flat_rec_list = list(dict.fromkeys([item for sublist in rec_movies_list_all for item in sublist]))  # flat list of recommended movies
-        # print(rec_movies_list_all)
+        # print(rec_movies_list_all)'''
